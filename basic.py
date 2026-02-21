6b4f5507a32051b2f6d0479a09307783516bbd17
Vanisha Tiwari <tiwarivanishagpt@gmail.com>
2026-02-21 13:40:39 +0530

    added code for factorial

--- basic.py
+++ basic.py
@@ -2,4 +2,15 @@ name = "Vanisha"
 birthday = 2
 print(name)
 a = int()
-print(a)
+print(a)   
+
+
+print("Enter a number: ")
+n = input ()
+n = int(n)
+fact = 1
+i = 1 
+while n >= i:
+    fact = fact * i
+    i = i+1
+print (fact)
